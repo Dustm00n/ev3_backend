edd300d6b9e8ff285436b5c72cbd5aa44d4470bc
Dustm00n <Gerardoserranosoto@gmail.com>
2024-12-22 11:57:09 -0300

    se modifica index principal

--- api/v1/config/auth.php
+++ api/v1/config/auth.php
@@ -9,6 +9,7 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     http_response_code(405); 
+    
     echo json_encode(['error' => 'Only GET requests are allowed']);
     exit;
 }
--- api/v1/index.php
+++ api/v1/index.php
@@ -4,10 +4,10 @@ require_once 'basicInfo.php';
 require_once 'services.php';
 require_once 'about.php';
 require_once 'contacto-rrss/cotactoController.php';
-require_once 'quienes-somos/quienesController.php'; // Importar funciones de quienes-somos
-///
+require_once 'quienes-somos/quienesController.php';
 require_once 'estilo-visual/estilo-visual.php';
 require_once 'productos-servicios/productos-servicios.php';
+
 // Obtener la ruta solicitada
 $requestUri = $_SERVER['REQUEST_URI'];
 $path = parse_url($requestUri, PHP_URL_PATH);
@@ -62,6 +62,52 @@ if (isset($pathSegments[3])) {
                     break;
                 case 'PATCH':
                     $data = enableContactInfo();
+                    break;
+                default:
+                    http_response_code(405);
+                    $data = ['error' => 'Method Not Allowed'];
+                    break;
+            }
+            break;
+        case 'estilo-visual':
+            switch ($method) {
+                case 'GET':
+                    handle_estilo_visual_get();
+                    break;
+                case 'POST':
+                    handle_estilo_visual_post();
+                    break;
+                case 'PUT':
+                    handle_estilo_visual_put();
+                    break;
+                case 'DELETE':
+                    handle_estilo_visual_delete();
+                    break;
+                case 'PATCH':
+                    handle_estilo_visual_patch();
+                    break;
+                default:
+                    http_response_code(405);
+                    $data = ['error' => 'Method Not Allowed'];
+                    break;
+            }
+            break;
+        case 'productos-servicios':
+            switch ($method) {
+                case 'GET':
+                    handle_productos_servicios_get();
+                    break;
+                case 'POST':
+                    handle_productos_servicios_post();
+                    break;
+                case 'PUT':
+                    handle_productos_servicios_put();
+                    break;
+                case 'DELETE':
+                    handle_productos_servicios_delete();
+                    break;
+                case 'PATCH':
+                    handle_productos_servicios_patch();
                     break;
                 default:
                     http_response_code(405);
